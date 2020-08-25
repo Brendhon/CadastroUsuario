ba9dce519ddc18b46eb1dd09f012d041a5b76b90
Brendhon <brendhon.e.c.m@gmail.com>
2020-08-25 17:29:47 -0300

    add: Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 export default props =>
-    <aside className="Logo">
-        Logo
+    <aside className="logo">
+        <a href="/" className="logo">
+            <img src = {logo} alt="logo"></img>
+        </a>
     </aside>
