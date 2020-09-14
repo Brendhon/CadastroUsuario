05d1b5458f685e422f31dde218661ace3bcd25c1
Brendhon <brendhon.e.c.m@gmail.com>
2020-09-14 14:24:21 -0300

    start: Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i>
+        </span>
     </footer>
